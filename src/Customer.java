7f3d24d4728f1cce6d616caf574129efa0dd37dc
Prize-winner <3220016928@qq.com>
2019-06-15 00:03:33 +0800

    Intellij创建java小demo

---
+++ src/Customer.java
@@ -0,0 +1,10 @@
+/**
+ * @author 宁三水
+ * @version 1.0
+ * @descript PACKAGE_NAME
+ * @date 2019/6/10
+ */
+
+
+public class Customer {
+}
---
+++ src/HelloJava.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class HelloJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!"); //ctrl+d  复制一行  ctrl+y删除
+
+        Date date = new Date(); //alt+enter 自动补全导包
+
+        //shift+F6 快速重命名
+
+
+    }
+}
